a3ea5eeb876c67b00d860eb20be98c5ef781e555
Dmytro Protsenko <dmytro.protsenko@gmail.com>
2020-01-03 01:26:55 +0200

    add purchase section

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $('.services-tabs-title').click(function() {
   $('.services-tabs-title').removeClass('active');
   $('.services-tabs-content').removeClass('open');
+  $('.services-tabs-title-caret').removeClass('visible');
   $('.services-tabs-content').eq($('.services-tabs-title').index(this)).addClass('open');
+  $('i', this).addClass('visible');
   $(this).addClass('active');
 });
